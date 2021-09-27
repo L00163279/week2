9bbd28e7643fdf3cf01bbcf49e664e8f10361ab1
MShafeeq <L00163279@student.lyit.ie>
2021-09-27 10:33:00 +0100

    week2

---
+++ selection_demo/sample_if.py
@@ -0,0 +1,16 @@
+# .............................
+
+# File: .py
+# Author - Muhammed Shafeeq Thottathil
+# Version:
+# Created Date:  
+# Modified Date: 
+# Description :
+# Listening : 
+
+# ...............................
+
+if __name__ == '__main__':
+    grade = 50
+    print(grade)
+    if grade >= 40 : print("you passed")
