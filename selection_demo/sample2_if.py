986f36cdee80c4ae2e99a37571db4570c8b1d74f
MShafeeq <L00163279@student.lyit.ie>
2021-09-29 19:56:51 +0100

    commit!!

---
+++ selection_demo/sample2_if.py
@@ -0,0 +1,19 @@
+# .............................
+
+# File: .py
+# Author - Muhammed Shafeeq Thottathil
+# Version:
+# Created Date:  
+# Modified Date: 
+# Description :
+# Listening : 
+
+# ...............................
+
+if __name__ == '__main__':
+    grade = 50
+    module = "Python"
+
+    if (grade >= 70) and module == "Python":
+        print("you have a distinction!!!")
+
